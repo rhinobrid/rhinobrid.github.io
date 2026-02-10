a0f49930510114726f2617ab176249ce83236921
Ray BG <251700376+rhinobrid@users.noreply.github.com>
2026-02-10 12:10:00 +0000

    edit about page. disable waline comment system

--- src/site.config.ts
+++ src/site.config.ts
@@ -153,7 +153,7 @@ export const integ: IntegrationUserConfig = {
   },
   // Comment system
   waline: {
-    enable: true,
+    enable: false,
     // Server service link
     server: 'https://astro-theme-pure-waline.arthals.ink/',
     // Show meta info for comments
